a2d96a10db76c8d144cf50844b9d93fe9923fd57
hollowdll <pixerchat@gmail.com>
2023-09-09 16:37:53 +0300

    receipt model

---
+++ src/services/receipt/ReceiptApi/Models/Receipt.cs
@@ -0,0 +1,23 @@
+using CustomerApi.Models;
+using TicketApi.Models;
+
+namespace ReceiptApi.Models;
+
+public class Receipt
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+    public int CustomerId { get; set; }
+    public long TicketId { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public Receipt(Customer customer, Ticket ticket, int customerTicketCount)
+    {
+        Message = $@"Customer {customer.FirstName} {customer.LastName}
+        generated a ticket with code {ticket.Code} at {ticket.CreatedAt}.
+        They have generated {customerTicketCount} tickets in total.";
+        CustomerId = customer.Id;
+        TicketId = ticket.Id;
+        CreatedAt = DateTime.Now;
+    }
+}
