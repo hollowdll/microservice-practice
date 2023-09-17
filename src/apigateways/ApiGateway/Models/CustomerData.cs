9a1de5f4e4e38336742601f0c2db1a346f3edad0
hollowdll <pixerchat@gmail.com>
2023-09-17 18:44:13 +0300

    api gateway customer models

---
+++ src/apigateways/ApiGateway/Models/CustomerData.cs
@@ -0,0 +1,10 @@
+namespace ApiGateway.Models;
+
+public class CustomerData
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public DateTime CreatedAt { get; set; }
+}
