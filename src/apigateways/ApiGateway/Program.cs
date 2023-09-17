ff8b3515abc9b1e351fac86280fd3ba8ee04babd
hollowdll <pixerchat@gmail.com>
2023-09-17 19:41:46 +0300

    configure urls

--- src/apigateways/ApiGateway/Program.cs
+++ src/apigateways/ApiGateway/Program.cs
@@ -1,3 +1,5 @@
+using ApiGateway.Config;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<UrlsConfig>(builder.Configuration.GetSection("urls"));
 
 var app = builder.Build();
 
