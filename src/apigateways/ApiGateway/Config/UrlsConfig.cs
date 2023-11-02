44bd6208d8e51987638ec3da3296fb1aeba3ef28
hollowdll <pixerchat@gmail.com>
2023-11-02 21:49:42 +0200

    setup customer grpc client

--- src/apigateways/ApiGateway/Config/UrlsConfig.cs
+++ src/apigateways/ApiGateway/Config/UrlsConfig.cs
@@ -29,4 +29,6 @@ public class UrlsConfig
     public string Customer { get; set; } = null!;
     public string Ticket { get; set; } = null!;
     public string Receipt { get; set; } = null!;
+
+    public string GrpcCustomer { get; set; } = null!;
 }
--- src/apigateways/ApiGateway/Extensions/Extensions.cs
+++ src/apigateways/ApiGateway/Extensions/Extensions.cs
@@ -1,13 +1,31 @@
+using ApiGateway.Config;
 using ApiGateway.Services;
+using GrpcCustomer;
+using Microsoft.Extensions.Options;
 
 internal static class Extensions
 {
+    // Setup HTTP clients
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         // These are for sending HTTP requests to different services.
         services.AddHttpClient<ICustomerService, CustomerHttpService>();
         services.AddHttpClient<ITicketService, TicketHttpService>();
         services.AddHttpClient<IReceiptService, ReceiptHttpService>();
+
+        return services;
+    }
+
+    // Setup gRPC services and clients
+    public static IServiceCollection AddGrpcServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICustomerService, CustomerGrpcService>();
+
+        services.AddGrpcClient<Customer.CustomerClient>((services, options) =>
+        {
+            var customerApi = services.GetRequiredService<IOptions<UrlsConfig>>().Value.GrpcCustomer;
+            options.Address = new Uri(customerApi);
+        });
 
         return services;
     }
